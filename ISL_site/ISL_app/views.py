5e7f175029b775fd28013ca4ddeaf5de74c82b2e
MAYUKH P V <pvmayukh>
2021-04-22 00:00:53 +0530

    template design

--- ISL_site/ISL_app/views.py
+++ ISL_site/ISL_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import *
+
 
 # Create your views here.
+def camera(request):
+    return render(request, "camera.html")
+def index(request):
+    return render(request, "index.html")
+def inner(request):
+    return render(request, "inner-page.html")
+
--- ISL_site/ISL_site/settings.py
+++ ISL_site/ISL_site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ISL_app.apps.IslAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'ISL_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR,'template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
